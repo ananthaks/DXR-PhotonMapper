cd6e2eac75831d312d083af65cdc295671a28a97
ananthaks <anantha.aks123@gmail.com>
2018-11-19 12:24:45 -0500

    Updated Cover Image

--- PhotonMapper/PhotonMapper/Main.cpp
+++ PhotonMapper/PhotonMapper/Main.cpp
@@ -16,6 +16,6 @@
 _Use_decl_annotations_
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
-    PhotonMapperRenderer sample(1280, 720, L"D3D12 Raytracing - Simple Lighting");
+    PhotonMapperRenderer sample(1280, 720, L"DXR Photon Mapping");
     return Win32Application::Run(&sample, hInstance, nCmdShow);
 }
